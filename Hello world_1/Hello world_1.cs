b67491a4ab3694a003f1e448ec26fa819fd77887
bachkippe1 <valentinrittmann@me.com>
2023-12-05 14:18:33 +0100

    Add Hello World

--- Hello world_1/Hello world_1.cs
+++ Hello world_1/Hello world_1.cs
@@ -17,7 +17,7 @@ namespace Hello_world_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	
+			engine.GenerateInformation("Hello World");
 		}
 	}
 }
